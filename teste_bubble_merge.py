99e3ffc05d0ccc93a3bdf13993ed0a6fc57c5702
Cássio Cavalcante <cassiolcavalcante@gmail.com>
2023-11-30 10:28:27 -0300

    Add files via upload

---
+++ teste_bubble_merge.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from scipy.stats import ttest_ind , t
+
+
+caminho_do_csv = r"C:\Users\hvell\Desktop\cesupa\.py\teste_hip\dados_do_banco.csv"
+df = pd.read_csv(caminho_do_csv)
+
+algoritmo1 = 'Bubble Sort'
+algoritmo2 = 'Merge Sort'
+
+
+dados_algoritmo1 = df[df['nome_algoritmo'] == algoritmo1]['temp_exec']
+dados_algoritmo2 = df[df['nome_algoritmo'] == algoritmo2]['temp_exec']
+
+
+statistic, p_value = ttest_ind(dados_algoritmo1, dados_algoritmo2)
+graus_de_liberdade = len(dados_algoritmo1) + len(dados_algoritmo2) - 2
+
+# Nível de significância
+alpha = 0.05
+
+# Intervalo de confiança
+intervalo_confianca = t.interval(1 - alpha, graus_de_liberdade, loc=dados_algoritmo1.mean() - dados_algoritmo2.mean(),
+                                 scale=((dados_algoritmo1.std()**2 / len(dados_algoritmo1)) + (dados_algoritmo2.std()**2 / len(dados_algoritmo2)))**0.5)
+
+print(f"Intervalo de Confiança para a Diferença das Médias: {intervalo_confianca}")
+
+alpha = 0.05
+if p_value < alpha:
+    print(f"Rejeitar a hipótese nula. Há evidência suficiente para sugerir que os tempos de execução de {algoritmo1} e {algoritmo2} são diferentes.")
+else:
+    print(f"Não há evidência suficiente para sugerir que os tempos de execução de {algoritmo1} e {algoritmo2} são diferentes.")
+
